49b3056afb210d4a8e4a371b436f6125527b1b4e
Leon Lee <93leonlee@gmail.com>
2014-02-15 15:00:47 -0500

    add calibration to sensor

--- src/com/penapps/rotapong/TestRotationActivity.java
+++ src/com/penapps/rotapong/TestRotationActivity.java
@@ -9,6 +9,8 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class TestRotationActivity extends Activity implements SensorEventListener {
@@ -24,6 +26,9 @@ public class TestRotationActivity extends Activity implements SensorEventListene
 	private float[] mTempRotation, mRotation;
 	
 	private TextView mTextView;
+	private Button calibrate;
+	private boolean calibrated = true;
+	private float calibrationZ = 0.0f;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,14 @@ public class TestRotationActivity extends Activity implements SensorEventListene
 		mRotation = new float[ROTATION_SIZE];
 		
 		mTextView = (TextView)findViewById(R.id.text);
+		calibrate = (Button)findViewById(R.id.calibrate);
+		
+		calibrate.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				calibrated = false;
+			}
+		});
 	}
 	
 	@Override
@@ -72,8 +85,10 @@ public class TestRotationActivity extends Activity implements SensorEventListene
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		if (event.accuracy < SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM)
+		if (event.accuracy < SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM){
+			Log.d(TAG, "Inaccurate");
 			return;
+		}
 		
 		float[] array = null;
 		switch (event.sensor.getType())
@@ -87,19 +102,24 @@ public class TestRotationActivity extends Activity implements SensorEventListene
         default:
         	return;
 		}
+		Log.i(TAG, event.values[0] + " " + event.values[1] + " " + event.values[2]);
 		System.arraycopy(event.values, 0, array, 0, 3);
 		if (!(Float.isNaN(mGravity[0]) || Float.isNaN(mGeomagnetic[0]))
 			&& SensorManager.getRotationMatrix(mTempRotation, null, mGravity, mGeomagnetic))
-        {
+        {	
 			// Warp coordinate system to ease conversion to game world
 			SensorManager.remapCoordinateSystem(mTempRotation, SensorManager.AXIS_Y, SensorManager.AXIS_Z, mRotation);
 			SensorManager.getOrientation(mRotation, mOrientation);
-			String msg = "Orientation vector in degrees: z: " + (int)Math.toDegrees(mOrientation[0])
+			String msg = "Orientation vector in degrees: z: " + (int)Math.toDegrees(mOrientation[0] - calibrationZ)
 				+ " x: " + (int)Math.toDegrees(mOrientation[1])
 				+ " y: " + (int)Math.toDegrees(mOrientation[2]);
 			mTextView.setText(msg);
 			Log.i(TAG, msg);
+			
+			if (!calibrated){
+				calibrationZ = mOrientation[0];
+				calibrated = true;
+			}			
         }
 	}
-
 }
